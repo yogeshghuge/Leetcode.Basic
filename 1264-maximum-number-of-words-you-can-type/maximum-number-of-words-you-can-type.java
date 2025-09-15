c48750a5c8f761524ee228a6b88f924e11389340
Yogesh Ghuge <159008201+yogeshghuge@users.noreply.github.com>
2025-09-15 11:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (62.65%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-       int ans = 0;
-    boolean[] broken = new boolean[26];
-
-    for (final char c : brokenLetters.toCharArray())
-      broken[c - 'a'] = true;
-
-    for (final String word : text.split(" "))
-      ans += canBeTyped(word, broken);
-
-    return ans;
-  }
-
-  private int canBeTyped(final String word, boolean[] broken) {
-    for (final char c : word.toCharArray())
-      if (broken[c - 'a'])
-        return 0;
-    return 1; 
+   static {
+        for(int i = 0; i < 500; i++) canBeTypedWords("jjhc", "ndc");
     }
+    public static int canBeTypedWords(String text, String brokenLetters) {
+        int count = 1;
+        boolean flag = true;
+        boolean[] c = new boolean[26];
+        for(int i = 0; i < brokenLetters.length(); i++) c[brokenLetters.charAt(i) - 97] = true;
+        for(char ch : text.toCharArray()) {
+            if(ch == ' ') {
+                flag = true;
+                count++;
+            }
+            else if(c[ch - 97] && flag) {
+                count--;
+                flag = false;
+            }
+        }
+        return count < 1 ? 0 : count;
+    }    
 }
