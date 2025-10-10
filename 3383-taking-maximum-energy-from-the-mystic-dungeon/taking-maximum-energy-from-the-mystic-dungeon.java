6d339af4db2a26b28b224216e3f81fe7072904fd
Yogesh Ghuge <159008201+yogeshghuge@users.noreply.github.com>
2025-10-10 17:14:07 +0530

    Time: 9 ms (14.48%) | Memory: 63.5 MB (15.17%) - LeetSync

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+    int[] dp = energy.clone();
+    for (int i = energy.length - 1 - k; i >= 0; --i)
+      dp[i] += dp[i + k];
+    return Arrays.stream(dp).max().getAsInt();
+        
+    }
+}
